9221e95afeda49625bf9c898baf5265877d64894
jadedanial <danialjade@gmail.com>
2023-07-11 20:26:55 +0300

    Added requirements.txt

--- backend/build.sh
+++ backend/build.sh
@@ -5,6 +5,5 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
-python manage.py createsuperuser --noinput
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME
 python manage.py migrate
