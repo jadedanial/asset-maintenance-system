7965a47b2e19b31dbda0025924a0bf7e0af002de
jadedanial <danialjade@gmail.com>
2023-07-11 18:48:18 +0300

    Added requirements.txt

--- backend/DanialSoftAMS/settings.py
+++ backend/DanialSoftAMS/settings.py
@@ -126,11 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
