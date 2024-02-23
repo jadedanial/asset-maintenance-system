5360261734595301563116d7562ffa81ad3c8e4e
Jade Danial <danialjade@gmail.com>
2024-02-23 21:43:03 +0300

    Deploy backend to Vercel.

--- backend/ams/apps.py
+++ backend/ams/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError
 
 
 class AmsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "ams"
+
+    def ready(self):
+        try:
+            User = get_user_model()
+            if not User.objects.filter(is_superuser=True).exists():
+                User.objects.create_superuser(
+                    'jadedanial', 'jadedanial@ams.com', 'assetmaintenancesystem')
+        except OperationalError:
+            pass
