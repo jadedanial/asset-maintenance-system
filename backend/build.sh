381cf6fe48773419dbe86f41d5d6c94a0a4b6081
Jade Danial <danialjade@gmail.com>
2024-02-23 18:34:34 +0300

    Deploy backend to Vercel.

--- backend/build.sh
+++ backend/build.sh
@@ -1,8 +1,2 @@
-#!/usr/bin/env bash
-
-set -o errexit  # exit on error
-
 pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3.9 manage.py collectstatic --noinput
