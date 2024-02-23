ce55b69cad8e4c4fa3d61f18d4476c0aa4ab56f4
Jade Danial <danialjade@gmail.com>
2024-02-23 21:04:31 +0300

    Deploy backend to Vercel.

--- backend/DanialSoftAMS/settings.py
+++ backend/DanialSoftAMS/settings.py
@@ -92,14 +92,8 @@ WSGI_APPLICATION = "DanialSoftAMS.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
-
-DATABASES["default"] = dj_database_url.config()
+DATABASES = {'default': dj_database_url.config(
+    default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))}
 
 
 # Password validation
