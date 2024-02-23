77bab180fd849aebee7bd5993271e9558f12ec55
Jade Danial <danialjade@gmail.com>
2024-02-23 20:37:40 +0300

    Deploy backend to Vercel.

--- backend/DanialSoftAMS/settings.py
+++ backend/DanialSoftAMS/settings.py
@@ -99,8 +99,7 @@ DATABASES = {
     }
 }
 
-prod_db = dj_database_url.config(conn_max_age=600)
-DATABASES["default"].update(prod_db)
+DATABASES["default"] = dj_database_url.config()
 
 
 # Password validation
