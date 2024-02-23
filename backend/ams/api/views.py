60796d3b52041ec495d079f6752bca43d954c914
Jade Danial <danialjade@gmail.com>
2024-02-24 00:08:45 +0300

    Deploy frontend to Vercel.

--- backend/ams/api/views.py
+++ backend/ams/api/views.py
@@ -146,7 +146,13 @@ class LoginView(APIView):
         token = jwt.encode(payload, "secret", algorithm="HS256")
         response = Response()
         response.set_cookie(
-            key="jwt", value=token, domain='https://asset-maintenance-system-frontend.vercel.app')
+            key="jwt",
+            value=token,
+            domain='https://asset-maintenance-system-frontend.vercel.app',
+            path='/',
+            secure=True,
+            httponly=True
+        )
         response.data = {"jwt": token}
 
         return response
