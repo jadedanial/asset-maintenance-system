81990c64dc059355d3921b3b44a266ac4e1714f3
Jade Danial <danialjade@gmail.com>
2024-02-23 20:13:48 +0300

    Deploy backend to Vercel.

--- backend/build.sh
+++ backend/build.sh
@@ -1,2 +1,9 @@
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Make migrations..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collecting static files..."
+python3.9 manage.py collectstatic --noinput --clear
