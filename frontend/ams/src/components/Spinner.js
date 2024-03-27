b3ebd1f9335855c634e59714fcfbc1611e206621
Jade Danial <danialjade@gmail.com>
2024-03-27 18:42:29 +0300

    Added loading spinner.

--- frontend/ams/src/components/Spinner.js
+++ frontend/ams/src/components/Spinner.js
@@ -11,7 +11,7 @@ const Spinner = ({ theme }) => {
         className="justified-row align-items-center"
         style={{
           height: "100%",
-          background: theme === "light" ? "#f0ffff" : "#000c17",
+          background: theme === "light" ? "#f8f9fa" : "#161d40",
         }}
       >
         <img
