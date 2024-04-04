f17b3cdc21a87c24ec5254d2bc75fdd8327c8f79
Jade Danial <danialjade@gmail.com>
2024-04-04 20:32:02 +0300

    Debug transact module query.

--- frontend/ams/src/components/AddUpdateItem.js
+++ frontend/ams/src/components/AddUpdateItem.js
@@ -228,11 +228,11 @@ const AddUpdateItem = ({
       })
       .catch((err) => {
         console.log(err.response.data[0]);
+        setSubmit(false);
         setSuccess(false);
         setLabel(err.response.data[0]);
         setColor("#ff0000");
       });
-    setSubmit(false);
   };
 
   const { mutate } = useMutation(createItem);
