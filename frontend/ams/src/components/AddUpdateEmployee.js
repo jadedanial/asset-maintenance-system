16a069cdda9dcd444adc9aac050556c439baed47
Jade Danial <danialjade@gmail.com>
2024-04-06 22:33:58 +0300

    Debug transact module query.

--- frontend/ams/src/components/AddUpdateEmployee.js
+++ frontend/ams/src/components/AddUpdateEmployee.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useCustomQueryClient } from "../useQueryClient";
 import { useMutation } from "react-query";
 import axios from "axios";
 import {
@@ -48,6 +49,7 @@ const AddUpdateEmployee = ({
   onCloseDrawer,
   theme,
 }) => {
+  const queryClient = useCustomQueryClient();
   const dateFormat = "YYYY-MM-DD";
   const displayDateFormat = "MMMM DD, YYYY";
   const datePickerFormat = (value) => `${value.format("MMMM DD, YYYY")}`;
@@ -217,35 +219,45 @@ const AddUpdateEmployee = ({
       emp_section: employeeSection,
     };
     const token = sessionStorage.getItem("token");
-    axios({
-      method: updateData ? "PATCH" : "POST",
-      url: `${process.env.REACT_APP_API_URL}/api/employee`,
-      data: employeeData,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Token ${token}`,
-      },
-      withCredentials: true,
-    })
-      .then((response) => {
-        setEmployeeID(response.data["emp_id"]);
-        if (updateData) {
-          getSection();
-        }
-        setLoading(false);
-        setSuccess(true);
-      })
-      .catch((err) => {
-        console.log(err.response.data[0]);
-        setSubmit(false);
-        setSuccess(false);
+    try {
+      const response = await axios({
+        method: updateData ? "PATCH" : "POST",
+        url: `${process.env.REACT_APP_API_URL}/api/employee`,
+        data: employeeData,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${token}`,
+        },
+        withCredentials: true,
       });
+      setEmployeeID(response.data["emp_id"]);
+      if (updateData) {
+        getSection();
+      }
+      setSuccess(true);
+    } catch (err) {
+      console.log(err.response.data[0]);
+      setSubmit(false);
+      setSuccess(false);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  const { mutate } = useMutation(createEmployee);
+  const { mutate } = useMutation(createEmployee, {
+    onSuccess: () => {
+      //queryClient.invalidateQueries("employees");
+    },
+  });
 
   const onFinish = () => {
-    mutate();
+    try {
+      mutate();
+      // Handle any post-mutation actions here
+    } catch (error) {
+      console.error("Mutation error:", error);
+      // Handle the error appropriately
+    }
   };
 
   return (
